4f08251d2ae4566635c403abdb547aa76db97d5e
charlespacker <charles_packer@yahoo.com>
2021-04-13 07:36:24 -0700

    Adding 3 params

--- src/ethsign/ethsign.go
+++ src/ethsign/ethsign.go
@@ -341,6 +341,10 @@ func main() {
         value := math.MustParseBig256(c.String("value"))
         chainID := math.MustParseBig256(c.String("chain-id"))
 
+		feecurrency := math.MustParseBig256(c.String("feecurrency"))
+        gatewayfeerecipient := math.MustParseBig256(c.String("gatewayfeerecipient"))
+        gatewayfee := math.MustParseBig256(c.String("gatewayfee"))
+
         dataString := c.String("data")
         if dataString == "" {
           dataString = "0x"
@@ -353,7 +357,7 @@ func main() {
 
         var tx *types.Transaction
         if create {
-          tx = types.NewContractCreation(nonce, value, gasLimit, gasPrice, data)
+          tx = types.NewContractCreation(nonce, value, gasLimit, gasPrice, data,feecurrency,gatewayfeerecipient,gatewayfee)
         } else {
           tx = types.NewTransaction(nonce, to, value, gasLimit, gasPrice, data)
         }
