6c9687069a0935857f36d205ab1598faa356db65
charlespacker <charles_packer@yahoo.com>
2021-04-13 07:37:56 -0700

    Update spacing

--- src/ethsign/ethsign.go
+++ src/ethsign/ethsign.go
@@ -357,9 +357,9 @@ func main() {
 
         var tx *types.Transaction
         if create {
-          tx = types.NewContractCreation(nonce, value, gasLimit, gasPrice, data,feecurrency,gatewayfeerecipient,gatewayfee)
+          tx = types.NewContractCreation(nonce, value, gasLimit, gasPrice, data, feecurrency, gatewayfeerecipient, gatewayfee)
         } else {
-          tx = types.NewTransaction(nonce, to, value, gasLimit, gasPrice, data)
+          tx = types.NewTransaction(nonce, to, value, gasLimit, gasPrice, data, feecurrency, gatewayfeerecipient, gatewayfee)
         }
 
         signed, err := wallet.SignTxWithPassphrase(*acct, passphrase, tx, chainID)
