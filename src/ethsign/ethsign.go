49bf53f73b5f2cea427e06863e8857ceca732763
charlespacker <charles_packer@yahoo.com>
2021-04-25 08:44:54 -0700

    add repo v

--- src/ethsign/ethsign.go
+++ src/ethsign/ethsign.go
@@ -13,10 +13,10 @@ import (
 
   "github.com/ethereum/go-ethereum/common"
   "github.com/ethereum/go-ethereum/common/math"
-  "github.com/ethereum/go-ethereum/common/hexutil"
+  "github.com/celo-org/celo-blockchain/common/hexutil"
   "github.com/ethereum/go-ethereum/accounts"
   "github.com/ethereum/go-ethereum/accounts/keystore"
-  "github.com/celo-org/celo-blockchain/accounts/usbwallet"
+  "github.com/ethereum/go-ethereum/accounts/usbwallet"
   "github.com/celo-org/celo-blockchain/core/types"
   "github.com/celo-org/celo-blockchain/crypto"
   "github.com/ethereum/go-ethereum/rlp"
